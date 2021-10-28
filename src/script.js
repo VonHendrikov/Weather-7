6364cc2de77772ce5238edbc3ecc78083b35847c
VonHendrikov <88203580+VonHendrikov@users.noreply.github.com>
2021-10-28 15:50:31 +0100

    Axios request for daily forecast

--- src/script.js
+++ src/script.js
@@ -60,6 +60,15 @@ form.addEventListener("submit", handleSubmit);
 //button.addEventListener("click", getCurrentPosition);
 
 //CURRENT WEATHER ATTRIBUTES AND DESCRIPTORS
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "16fe8924e5a075000ef90dc44bff1cd0";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
+}
+
 function showTemperature(response) {
   console.log(response.data);
 
@@ -86,6 +95,8 @@ function showTemperature(response) {
     "alt",
     `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 //TEMPERATURE CONVERSIONS
@@ -127,7 +138,10 @@ celsiusLink.addEventListener("click", showCelsiusTemperature);
 //}
 
 //FORECAST DATA
-function displayForecast() {
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
